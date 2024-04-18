c6525d0265d3d36882035a36b43d07d0f3aa8c4e
FelipeSamassa <137512643+FelipeSamassa@users.noreply.github.com>
2024-04-18 10:02:42 -0300

    atualização

--- ComparadorCerva-React/ComparadorCerva-React/src/Components/Cervejas.jsx
+++ ComparadorCerva-React/ComparadorCerva-React/src/Components/Cervejas.jsx
@@ -1,19 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import style from './Cervejas.module.css'
 
-const Cervejas = ({nome, imagem, num, ml}) => {
+const Cervejas = ({ nome, imagem, num, ml }) => {
+
+  const [count, setCount] = useState(0)
+  const [valor, setValor] = useState('')
+
+  const handleValor = (e) => {
+    e.preventDefault()
+
+    const updateValor = (e.target.value)
+
+    setValor(updateValor)
+  }
+
+  if(valor > 0) {
+
+  }
+
+
   return (
     <div className={style.containerUn}>
-        <h3>{nome}</h3>
-        <img src={imagem} alt="" />
-        <div>
-            <button>+</button>
-            <span>0</span>
-            <button>-</button>
-        </div>
-        <span>R$</span> 
-        <input type="number" placeholder={num}/>
+      <h3>{nome}</h3>
+      <img src={imagem} alt="" />
+      <div>
+        <button onClick={() => setCount(count > 0 ? count - 1 : 0)}>-</button>
+        <p>{count}</p>
+        <button onClick={() => setCount(count + 1)}>+</button>
+      </div>
+      <div>
+        <span>R$</span>
+        <input
+          type="number"
+          placeholder={num}
+          value={valor}
+          onChange={handleValor}
+        />
         <span>{ml}ml</span>
+      </div>
+      
     </div>
   )
 }
